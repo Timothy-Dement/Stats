734e3e1ed7e57d1c7db3a61b1dda5d276ab8282a
Timothy-Dement <tmdement@ncsu.edu>
2018-11-19 23:58:31 -0500

    Results

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -f ranks.txt
+touch ranks.txt
+
+for CODE_SMELL in "_data-class" "_feature-envy" "_god-class" "_long-method"
+    do
+        for METRIC in "f_score" "inform" "kappa" "pct_dth"
+        do
+            printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ranks.txt
+            cat ${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ranks.txt
+        done
+    done
