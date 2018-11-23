45954d30af5849cd126cab40264e6224beb5992e
Timothy-Dement <tmdement@ncsu.edu>
2018-11-23 17:17:06 -0500

    Add missing acc in run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ touch ranks-by-smell.txt ranks-by-metric.txt
 
 for CODE_SMELL in "_data-class" "_feature-envy" "_god-class" "_long-method"
     do
-        for METRIC in "f_score" "inform" "kappa" "pct_dth"
+        for METRIC in "acc" "f_score" "inform" "kappa" "pct_dth"
         do
             printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ranks-by-smell.txt
             cat _output/${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ranks-by-smell.txt
