d65e1883fd405451845caee228cb20805202a350
Timothy-Dement <tmdement@ncsu.edu>
2018-11-23 17:25:48 -0500

    Reorder

--- run.sh
+++ run.sh
@@ -5,14 +5,14 @@ touch ranks-by-smell.txt ranks-by-metric.txt
 
 for CODE_SMELL in "_data-class" "_feature-envy" "_god-class" "_long-method"
     do
-        for METRIC in "acc" "f_score" "inform" "kappa" "pct_dth"
+        for METRIC in "acc" "f_score" "kappa" "inform" "pct_dth"
         do
             printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ranks-by-smell.txt
             cat _output/${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ranks-by-smell.txt
         done
     done
 
-for METRIC in "acc" "f_score" "inform" "kappa" "pct_dth"
+for METRIC in "acc" "f_score" "kappa" "inform" "pct_dth"
     do
         for CODE_SMELL in "_data-class" "_feature-envy" "_god-class" "_long-method"
         do
