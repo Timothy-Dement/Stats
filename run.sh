2a3107488c6af809344518e98925e61c2c9d224d
Timothy-Dement <tmdement@ncsu.edu>
2018-11-30 16:41:21 -0500

    combine

--- run.sh
+++ run.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-rm -f ranks-by-smell.txt ranks-by-metric.txt
-touch ranks-by-smell.txt ranks-by-metric.txt
-
-ROOT="__smote"
-
-for CODE_SMELL in "_data-class" "_feature-envy" "_god-class" "_long-method"
+for ROOT in "__norm-r" "__smote-r" "__both-r"
     do
-        for METRIC in "acc" "f_score" "inform" "kappa" "pct_dth"
-        do
-            printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ranks-by-smell.txt
-            cat ${ROOT}/${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ranks-by-smell.txt
-        done
+        rm -rf ${ROOT}
+        mkdir ${ROOT}
+        touch ${ROOT}/ranks-by-smell.txt
+        touch ${ROOT}/ranks-by-metric.txt
     done
 
-for METRIC in "acc" "f_score" "inform" "kappa" "pct_dth"
+for ROOT in "__norm" "__smote"  "__both"
     do
-        for CODE_SMELL in "_data-class" "_feature-envy" "_god-class" "_long-method"
-        do
-            printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ranks-by-metric.txt
-            cat ${ROOT}/${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ranks-by-metric.txt
-        done
+        for CODE_SMELL in "cl-data-class" "cl-god-class" "ml-feature-envy" "ml-long-method"
+            do
+                for METRIC in "acc" "f_score" "kappa" "inform" "pct_dth"
+                do
+                    printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ${ROOT}-r/ranks-by-smell.txt
+                    cat ${ROOT}/${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ${ROOT}-r/ranks-by-smell.txt
+                done
+            done
+    done
+
+for ROOT in "__norm" "__smote" "__both"
+    do
+        for METRIC in "acc" "f_score" "kappa" "inform" "pct_dth"
+            do
+            for CODE_SMELL in "cl-data-class" "cl-god-class" "ml-feature-envy" "ml-long-method"
+                do
+                    printf "\n-- ${CODE_SMELL} -- ${METRIC} --\n" >> ${ROOT}-r/ranks-by-metric.txt
+                    cat ${ROOT}/${CODE_SMELL}/${CODE_SMELL}-${METRIC}.txt | python stats.py >> ${ROOT}-r/ranks-by-metric.txt
+                done
+            done
     done
